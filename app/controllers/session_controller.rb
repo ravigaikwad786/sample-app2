5729a19144e7e1b0e1217bfa907470d546f3f9fd
ravigaikwad123 <ravi.gaikwad@cryptextechnologies.com>
2021-09-29 18:45:25 +0530

    work done

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,10 +5,24 @@ class SessionController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-      if user &.authenticate(params[:session][:password])
+      if user && user.authenticate(params[:session][:password])
+        
+        if user.activated?
         log_in user
         params[:session][:remember_me]=='1' ? remember(user) : forget(user)
         redirect_to user
+        else
+        message = "Account Not Activated ."
+        message +="Check Your email for Activation link"
+        flash[:warning] = message
+        redirect_to root_url
+
+        #message = "Account not activated. "
+        #message += "Check your email for the activation link."
+        #flash[:warning] = message
+        #redirect_to root_url
+        end
+
       else
       flash.now[:danger] = 'Invalid email/password'  
       render 'new'
